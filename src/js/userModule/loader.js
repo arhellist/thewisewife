b42ece9f57e0c532ae882a23ac1323e4065931eb
arhellist <92224498+arhellist@users.noreply.github.com>
2024-11-17 17:45:37 +0400

    new Leave

--- src/js/userModule/loader.js
+++ src/js/userModule/loader.js
@@ -1,18 +1,7 @@
  export function loader(){
     document.querySelector('boby').insertAdjacentHTML(`beforebegin`,`
         <section class="loadPage flex flex-column">
-      <header class="loadPage_header flex flex-row">
-        <span class="loadPage_header_cancel">Cancel</span>
-        <div class="loadPage_header_headerTtitle flex flex-column">
-          <span class="headerTtitle_text">Мудрая жена</span>
-          <span class="headerTtitle_textBot">bot</span>
-        </div>
-        <div class="loadPage_header_menuIcon flex flex-row">
-          <div class="loadPage_header_menuIcon_point"></div>
-          <div class="loadPage_header_menuIcon_point"></div>
-          <div class="loadPage_header_menuIcon_point"></div>
-        </div>
-      </header>
+     
 
                
 
